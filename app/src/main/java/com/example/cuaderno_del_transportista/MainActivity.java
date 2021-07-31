09f552cfd6f42317d6eaf038a327689ae6425462
Fiz1965 <fizlozano@gmail.com>
2021-07-31 06:24:09 +0200

    comentario 3

--- app/src/main/java/com/example/cuaderno_del_transportista/MainActivity.java
+++ app/src/main/java/com/example/cuaderno_del_transportista/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comentario 2
+        //comentario 2 y 3
+
     }
 }
